131d620b99b860eb08ee806d19597ab76c6ece53
mclima <mcamil2009@gmail.com>
2024-04-14 17:07:30 -0400

    remove modal from index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import Modal from 'react-modal'
 import App from './App'
 import './index.css'
 
 const container = document.getElementById('root')
-Modal.setAppElement('#root')
 const root = createRoot(container)
 root.render(<App />)
